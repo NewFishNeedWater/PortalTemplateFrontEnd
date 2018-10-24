5a4882ca0b9b3a6268e13525efd7953ea2a74283
GLOBAL\I345149\Joey Yang <joey.yang02@sap.com>
2018-10-24 09:50:52 +0800

    first version

---
+++ servicerequests/Component.js
@@ -0,0 +1,256 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"sap/ui/model/odata/v2/ODataModel",
+	"sap/ui/model/json/JSONModel",
+	"ServiceRequests/model/models",
+	"ServiceRequests/controller/ListSelector",
+	"ServiceRequests/controller/ErrorHandler",
+    "ServiceRequests/controller/UtilityHandler",
+], function(UIComponent, Device, ODataModel, JSONModel, models, ListSelector, ErrorHandler, UtilityHandler) {
+	"use strict";
+
+	return UIComponent.extend("ServiceRequests.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		contactUUID: null,
+		contactID: null,
+		mockData: false,
+		SELECT_BOX_URLS: {
+			ServiceRequestLifeCycleStatusCode: '/ServiceRequestServiceRequestLifeCycleStatusCodeCollection',
+			ServicePriorityCode: '/ServiceRequestServicePriorityCodeCollection',
+			ServiceCategory: '/ServiceIssueCategoryCatalogueCategoryCollection',
+			IncidentCategory: '/ServiceIssueCategoryCatalogueCategoryCollection?$filter=ParentObjectID%20eq%20%27${0}%27',
+			DescriptionTypeCollection: '/ServiceRequestTextCollectionTypeCodeCollection',
+			ProductCategoryCollection: '/ProductCollection'
+		},
+
+		functionMetaData: {
+            ServiceRequestServicePriorityCodeCollection:null,
+            ServiceIssueCategoryCatalogueCategoryCollection:null,
+            ProductCollection:null,
+            IncidentModel:null
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * In this method, the device models are set and the router is initialized.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+            sap.ushell.Container = {
+
+                getUser: function () {
+
+                    return {
+                        getEmail: function () {
+                            return "joey.yang02@sap.com";
+
+                        },
+
+                    };
+                },
+                getHost : function(){
+                    return "http://127.0.0.1:4002/client"
+                }
+            };
+
+            this.utilityHandler = new UtilityHandler();
+            // Initial function metadata from back-end.
+            //this.getIncidentModelPromise();
+            this.getServiceIssueCategoryPromise();
+            this.getProductCollectionPromise();
+            this.getServiceRequestServicePriorityCodePromise();
+
+			if (window.location.href.indexOf("mockData") !== -1 || sap.ushell.Container.getUser().getEmail() === "") {
+				//this.mockData = true;
+				var model = new JSONModel(jQuery.sap.getModulePath("ServiceRequests") + "/mock/c4codata.json");
+				model.attachRequestCompleted(function() {
+					this.getData().ServiceRequestCollection.forEach(function(request) {
+						request.ServiceRequestDescription.forEach(function(description) {
+							description.CreatedOn = new Date(parseInt(description.CreatedOn.substring(description.CreatedOn.indexOf("(") + 1, description.CreatedOn.indexOf(")"))));
+						});
+					});
+				});
+				this.setModel(model);
+			} else {
+
+			    this.getServiceRequest();
+
+			}
+
+			this.oListSelector = new ListSelector();
+			this.startupParams = this.receiveStartupParams();
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// call the base component's init function and create the App view
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+
+        getServiceRequest : function(){
+
+            var model = new JSONModel();
+            var email = sap.ushell.Container.getUser().getEmail();
+
+            var url = sap.ushell.Container.getHost()+"/getServiceRequests?$skip=0&$top=20&$orderby=CreationDateTime desc&$filter=(ReporterEmail eq '" +  email + "' or ReporterEmail eq '" + email
+                + "') and (ServiceRequestUserLifeCycleStatusCodeText ne 'Completed' or ServiceRequestUserLifeCycleStatusCodeText ne 'Completed')&$expand=ServiceRequestDescription,ServiceRequestAttachmentFolder";
+
+           // alert(url);
+            $.ajax({
+                method: "GET",
+                url: url,
+                success: function(result) {
+                    if(result){
+                        result.forEach(function(oServiceRequest) {
+                            if(oServiceRequest.ServiceRequestDescription.length>0){
+                                oServiceRequest.ServiceRequestDescription.forEach(function(description) {
+                                    description.CreatedOn = new Date(parseInt(description.CreatedOn.substring(description.CreatedOn.indexOf("(") + 1, description.CreatedOn.indexOf(")"))));
+                                });
+                            }
+                        });
+                    }
+                    model.setData({"ServiceRequestCollection":result});
+                    model.refresh();
+
+                }.bind(this),
+                error: function(jqXHR) {
+                    var elm = jqXHR.responseXML.getElementsByTagName("message")[0];
+                    var error = elm.innerHTML || elm.textContent;
+                    MessageBox.error(error);
+                }
+            });
+
+            this.setModel(model);
+        },
+		getServiceRequestServicePriorityCodePromise: function(){
+            return new Promise(function(resolve, reject) {
+            	if(this.functionMetaData.ServiceRequestServicePriorityCodeCollection){
+            		resolve(this.functionMetaData.ServiceRequestServicePriorityCodeCollection)
+				}
+				this.utilityHandler.getModelReadPromise('/getServicePriorityCode').then(function(oData){
+                    this.functionMetaData.ServiceRequestServicePriorityCodeCollection = oData;
+                    resolve(this.functionMetaData.ServiceRequestServicePriorityCodeCollection)
+                }.bind(this));
+            }.bind(this));
+		},
+
+        getServiceIssueCategoryPromise: function(){
+            return new Promise(function(resolve, reject) {
+                if(this.functionMetaData.ServiceIssueCategoryCatalogueCategoryCollection){
+                    resolve(this.functionMetaData.ServiceIssueCategoryCatalogueCategoryCollection)
+                }
+                this.utilityHandler.getModelReadPromise('/getServiceCategory').then(function(oData){
+                    this.functionMetaData.ServiceIssueCategoryCatalogueCategoryCollection = oData;
+                    resolve(this.functionMetaData.ServiceIssueCategoryCatalogueCategoryCollection)
+				}.bind(this));
+            }.bind(this));
+        },
+
+        getProductCollectionPromise: function(){
+            return new Promise(function(resolve, reject) {
+                if(this.functionMetaData.ProductCollection){
+                    resolve(this.functionMetaData.ProductCollection)
+                }
+                this.utilityHandler.getModelReadPromise('/getProductCollection?$skip=0&$top=100').then(function(oData){
+                    this.functionMetaData.ProductCollection = oData;
+                    resolve(this.functionMetaData.ProductCollection)
+                }.bind(this));
+            }.bind(this));
+        },
+
+        getIncidentModelPromise: function(){
+            return new Promise(function(resolve, reject) {
+                if(this.functionMetaData.incidentModel){
+                    resolve(this.functionMetaData.incidentModel)
+                }
+                this.utilityHandler.getModelReadPromise('/getIncidentCategory').then(function(oData){
+                    this.functionMetaData.incidentModel = oData;
+                    resolve(this.functionMetaData.incidentModel)
+                }.bind(this));
+            }.bind(this));
+        },
+
+        createIncidentCategoryFilters: function(parentObject, typeCode) {
+            return [
+                new sap.ui.model.Filter({
+                    path: "ParentObjectID",
+                    operator: sap.ui.model.FilterOperator.EQ,
+                    value1: parentObject
+                }),
+                new sap.ui.model.Filter({
+                    path: "TypeCode",
+                    operator: sap.ui.model.FilterOperator.EQ,
+                    value1: typeCode
+                })
+            ];
+        },
+
+
+
+		receiveStartupParams: function() {
+			var obj = {},
+				oComponentData = this.getComponentData && this.getComponentData();
+
+			if (oComponentData && oComponentData.startupParameters) {
+				var startupParameters = oComponentData.startupParameters;
+				obj.createNewTicket = startupParameters.createNewTicket && startupParameters.createNewTicket[0];
+				obj.highPriority = startupParameters.highPriority && startupParameters.highPriority[0];
+				obj.pendingResponse = startupParameters.pendingResponse && startupParameters.pendingResponse[0];
+			}
+
+			return obj;
+		},
+
+		/**
+		 * The component is destroyed by UI5 automatically.
+		 * In this method, the ListSelector and ErrorHandler are destroyed.
+		 * @public
+		 * @override
+		 */
+		destroy: function() {
+			this.oListSelector.destroy();
+			if (!this.mockData) {
+				this._oErrorHandler.destroy();
+			}
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+
+		/**
+		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+		 * design mode class should be set, which influences the size appearance of some controls.
+		 * @public
+		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+		 */
+		getContentDensityClass: function() {
+			if (this._sContentDensityClass === undefined) {
+				// check whether FLP has already set the content density class; do nothing in this case
+				if (jQuery(document.body).hasClass("sapUiSizeCozy") || jQuery(document.body).hasClass("sapUiSizeCompact")) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		},
+
+		onConfigChange: function(oEvent) {
+			var settings = this.getMetadata().getManifest()["sap.cloud.portal"].settings;
+			this.getAggregation("rootControl").$().css("height", settings.widgetHeight.value + "px");
+		}
+	});
+
+});
---
+++ servicerequests/controller/UtilityHandler.js
@@ -0,0 +1,115 @@
+sap.ui.define([
+    "sap/ui/base/Object",
+    "sap/ui/model/FilterOperator"
+], function (UI5Object, FilterOperator) {
+    "use strict";
+
+    return UI5Object.extend("ServiceRequests.controller.UtilityHandler", {
+
+        /**
+         * Utility method: Read data from server and render oModel
+         * @param {JSONModel}oModel
+         * @param {string}baseURL
+         * @param {object}oSettings
+         *         ----{function} success: success call back method
+         *         ----{function} error: error call back method
+         *         ----{array} filters: filter table
+         */
+         oModelRead: function(oModel, baseURL, oSettings ){
+             var url = sap.ushell.Container.getHost()+baseURL;
+             if(this._checkURLWithCondition(oSettings)){
+                 url = url + "?";
+             }
+            if(oSettings && oSettings.filters){
+                 // In case need to add filter conditions
+                 url = this._setURLByFilters(url, oSettings.filters);
+             }
+            $.ajax({
+                url:url,
+                type:'GET',
+                async:false,
+                beforeSend: function(request) {
+                    //request.setRequestHeader("Authorization", chatbotAPI.nlAPIToken);
+                    request.setRequestHeader("Type", "application/json");
+                },
+                dataType:'json',
+                success: function(oData){
+                    if(oSettings && oSettings.success){
+                        oSettings.success(oData);
+                    }
+                }.bind(this),
+                error: function(jqXHR){
+                    // var elm = jqXHR.responseXML.getElementsByTagName("message")[0];
+                    // var error = elm.innerHTML || elm.textContent;
+                    // //MessageBox.error(error);
+                    // oSettings.error(error);
+                    if(oSettings && oSettings.error){
+                        oSettings.error(jqXHR);
+                    }
+                }
+            });
+        },
+
+        getModelReadPromise: function(baseURL, oSettings){
+            var url = sap.ushell.Container.getHost()+baseURL;
+            if(this._checkURLWithCondition(oSettings)){
+                url = url + "?";
+            }
+            if(oSettings && oSettings.filters){
+                // In case need to add filter conditions
+                url = this._setURLByFilters(url, oSettings.filters);
+            }
+             return new Promise(function(resolve, reject){
+                 $.ajax({
+                     url:url,
+                     type:'GET',
+                     beforeSend: function(request) {
+                         request.setRequestHeader("Type", "application/json");
+                     },
+                     dataType:'json',
+                     success: function(oData){
+                         resolve(oData);
+                     }.bind(this),
+                     error: function(jqXHR){
+                         reject(jqXHR);
+                     }
+                 });
+             });
+        },
+
+        /**
+         * Check if the url should has conditions
+         * @param oSettings
+         * @returns {boolean}
+         * @private
+         */
+        _checkURLWithCondition: function(oSettings){
+            if(!oSettings){
+                return false;
+            }
+            if(oSettings.filters){
+                return true;
+            }
+            return false;
+        },
+
+         _setURLByFilters: function(baseURL, filters){
+             if(!filters || filters.length === 0){
+                 return baseURL;
+             }
+             var i = 0, len = filters.length, str = '$filter=';
+             for(i = 0; i < len; i++){
+                 var filter = filters[i];
+                 if(filter.sOperator === FilterOperator.EQ){
+                     str += filter.sPath + ' eq ' + '\''+filter.oValue1+'\'';
+                 }
+                 if(i < len - 1){
+                     str = str + ' and '
+                 }
+             }
+             return baseURL + str;
+         },
+
+    });
+});
+
